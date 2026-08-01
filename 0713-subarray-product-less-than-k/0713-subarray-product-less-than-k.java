c136f963fe4e4b46bf6b2fff4a7bbdb0827a0c1b
G Hairsh <143336419+gharish-dev@users.noreply.github.com>
2026-08-01 23:21:38 +0530

    Time: 2 ms (99.97%), Space: 49.1 MB (50.45%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int count=0;
+        /*int count=0;
         for(int i=0;i<nums.length;i++){
             int pr=1;
             for(int j=i;j<nums.length;j++){
@@ -10,6 +10,18 @@ class Solution {
                 }
                 count++;
             }
+        }
+        return count;*/
+        // optimal
+              if (k <= 1) return 0;
+        int left=0,pr=1,count=0;
+        for(int i=0;i<nums.length;i++){
+            pr*=nums[i];
+            while(pr>=k){
+                pr/=nums[left];
+                left++;
+            }
+            count+=i-left+1;
         }
         return count;
         
