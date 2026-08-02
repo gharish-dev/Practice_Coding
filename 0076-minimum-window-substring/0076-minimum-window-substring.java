999373ee9f910f351ebe356c95b5ca4b41ff9b22
G Hairsh <143336419+gharish-dev@users.noreply.github.com>
2026-08-02 23:11:19 +0530

    Time: 16 ms (56.42%), Space: 47.5 MB (36.13%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) return "";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int start = 0;
+        int minLen = Integer.MAX_VALUE;
+        int required = t.length();
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                if (map.get(ch) > 0) {
+                    required--;
+                }
+                map.put(ch, map.get(ch) - 1);
+            }
+
+            while (required == 0) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if (map.get(leftChar) > 0) {
+                        required++;
+                    }
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
