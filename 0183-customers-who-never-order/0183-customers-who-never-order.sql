6411282bba9d9fba0014f037e071116c6b99c4fb
G Hairsh <143336419+gharish-dev@users.noreply.github.com>
2026-08-02 11:16:11 +0530

    Time: 667 ms (22.75%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,6 +1,5 @@
-SELECT name AS Customers
-FROM Customers
-WHERE id NOT IN (
-    SELECT customerId
-    FROM Orders
-);
+select name as Customers
+from Customers c
+left join Orders o
+on c.id=o.customerId 
+where o.customerId is null;
