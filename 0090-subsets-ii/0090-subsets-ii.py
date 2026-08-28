5dd63ddbde904bba6b4afa7f26c405c065f6f87c
G Hairsh <143336419+gharish-dev@users.noreply.github.com>
2026-08-28 23:44:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (89.1%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        result = []
+
+        def backtrack(start, path):
+            result.append(path[:])
+
+            for i in range(start, len(nums)):
+
+                # Skip duplicate choices at the same level
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+
+                path.append(nums[i])
+
+                backtrack(i + 1, path)
+
+                path.pop()
+
+        backtrack(0, [])
+
+        return result
